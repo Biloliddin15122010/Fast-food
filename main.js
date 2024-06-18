0b4e93c08ebef5b38696a9c480ccdbb734d6dbd8
Biloliddin <a@a.com>
2024-06-18 19:14:37 +0500

    comment_nomi

---
+++ main.js
@@ -0,0 +1,8 @@
+const dish = document.getElementById('dish');
+const dish1 = document.getElementById('dish1');
+const dish2 = document.getElementById('dish2');
+const dish3 = document.getElementById('dish3');
+const dish4 = document.getElementById('dish4');
+const dish5 = document.getElementById('dish5');
+
+
